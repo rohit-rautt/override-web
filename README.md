3bc622cfb4a58869c21dd100b3d7995898c8f8ae
rohit-rautt <rohitraut@csod.com>
2024-10-11 18:02:39 +0530

    override web instances with local server

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# override-web
+# Setting Up a JavaScript Script in Fish Shell
+
+This guide explains how to make your JavaScript script executable from anywhere in the Fish shell.
+
+## Steps
+
+1. **Make the Script Executable**
+
+   Run the following command to make your script executable:
+
+   ```javascript
+   chmod +x your_script.js
+   ```
+
+2. **Move the Script to the Bin Directory**
+
+   Move your script to the bin directory so that it can be accessed globally:
+
+   ```javascript
+   mv your_script.js /usr/local/bin/
+    ```
+
+3. **Source the CLI Configuration**
+
+   Finally, apply the changes to your Fish shell configuration:
+
+   ```javascript
+   source ~/.config/fish/config.fish
+   ```
+
+Now your JavaScript script should be executable from anywhere in the Fish shell!
+EOL
---
+++ html.js
@@ -0,0 +1,36 @@
+#!/usr/bin/env node
+
+const fs = require('fs').promises;
+
+const removeCspMetaTag = (html) => {
+    return html.replace(/<meta\s+http-equiv="Content-Security-Policy".*?\/?>\s*/i, '');
+};
+
+
+const replaceString = (html) => {
+    return html.replace(/\/dist-\d+-/g, 'http://localhost:8001/');
+};
+
+const processHtmlFile = async (filePath) => {
+    try {
+        const data = await fs.readFile(filePath, 'utf8');
+        let updatedHtml = removeCspMetaTag(data);
+        updatedHtml = replaceString(updatedHtml);
+        await fs.writeFile(filePath, updatedHtml, 'utf8');
+        console.log('File updated successfully!');
+    } catch (error) {
+        console.error(`Error processing the file: ${error.message}`);
+    }
+};
+
+// Get the file path from command line arguments
+const filePath = process.argv[2];
+const cleanFilePath = decodeURIComponent(filePath.replace(/^file:\/\//, ''));
+
+if (!cleanFilePath) {
+    console.error('Please provide a file path as an argument.');
+    process.exit(1);
+}
+
+// Run the processing function
+processHtmlFile(cleanFilePath);
